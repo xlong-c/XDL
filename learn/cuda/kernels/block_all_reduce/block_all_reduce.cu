9acf1b97261a8a616043ed9b91c21b99c6883954
xlong <starcang@qq.com>
2026-01-21 18:08:27 +0800

    更新: 修正.clangd文件路径并添加CUDA学习代码

---
+++ learn/cuda/kernels/block_all_reduce/block_all_reduce.cu
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cuda_fp8.h>
+#include <cuda_runtime.h>
+#include <float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <torch/extension.h>
+#include <torch/types.h>
+#include <vector>
+
+#define WARP_SIZE 32
+#define INT4(value) (reinterpret_cast<int4 *>(&(value))[0])
+#define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
+#define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
+#define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
+#define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
+
+template <const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum_f32(float val) {
+#pragma unroll
+  for (int mask = kWarpSize >> 1; mask >= 1; mask >> 1) {
+    val += __shfl_xor_sync(0xffffffff, val, mask);
+  }
+  return val;
+}
+
+template <const int NUM_THREADS = 256>
+__global__ void block_all_reduce_sum_f32_kernel(float *x, float *y, int mask) {
+  int tid = threadIdx.x;
+  int idx = NUM_THREADS * blockIdx.x + threadIdx.x;
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  __shared__ float reduce_smem[NUM_WARPS];
+  float sum =  (idx<mask){
+    
+  }
+}
