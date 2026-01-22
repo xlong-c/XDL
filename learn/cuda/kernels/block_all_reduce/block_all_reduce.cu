cc7a0a108a3844daa8749339f62e60a8f9c35336
xlong <starcang@qq.com>
2026-01-23 01:21:08 +0800

    u

--- learn/cuda/kernels/block_all_reduce/block_all_reduce.cu
+++ learn/cuda/kernels/block_all_reduce/block_all_reduce.cu
@@ -1,13 +1,8 @@
-#include <__clang_cuda_builtin_vars.h>
-#include <__clang_cuda_runtime_wrapper.h>
 #include <cuda_bf16.h>
 #include <cuda_fp16.h>
 #include <cuda_fp8.h>
 #include <cuda_runtime.h>
 #include <float.h>
-#include <torch/extension.h>
-#include <torch/types.h>
-#include <vector>
 
 #define WARP_SIZE 32
 #define INT4(value) (reinterpret_cast<int4 *>(&(value))[0])
@@ -124,7 +119,72 @@ __global__ void block_all_reduce_sum_f16x2_f32_kernel(half *x, half *y,
   constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
   __shared__ float smem[NUM_WARPS];
   half2 reg_x = HALF2(x[idx]);
-  half sum_x = (idx < mask) ? __hadd(reg_x.x, reg_x.y) : CUDART_ONE_FP16;
+  half sum = (idx < mask) ? __hadd(reg_x.x, reg_x.y) : CUDART_ONE_FP16;
   int warp = tid / WARP_SIZE;
   int lane = tid % WARP_SIZE;
+  sum = warp_reduce_sum_f16_f32(sum);
+  if (lane == 0) {
+    smem[warp] = sum;
+  }
+  __syncthreads();
+  sum = (lane < NUM_WARPS) ? smem[lane] : CUDART_ZERO_FP16;
+  if (warp == 0) {
+    sum = warp_reduce_sum_f16_f32(sum);
+  }
+  if (tid == 0) {
+    atomicAdd(y, sum);
+  }
+}
+
+template <const int NUM_THREADS = 256 / 2>
+__global__ void block_all_reduce_f16x2_f16_kernel(half *x, half *y, int mask) {
+  int tid = threadIdx.x;
+  int idx = (blockDim.x * NUM_THREADS + tid) * 2;
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  __shared__ float smem[NUM_WARPS];
+  half2 reg_x = HALF2(x[idx]);
+  half sum = idx < mask ? __hadd(reg_x.x, reg_x.y) : CUDART_ZERO_FP16;
+  sum = warp_reduce_sum_f16_f16(sum);
+  int warp_id = tid / WARP_SIZE;
+  int lane_id = tid % WARP_SIZE;
+  if (lane_id == 0) {
+    smem[warp_id] = sum;
+  }
+  __syncthreads();
+  // 满载线程是1024,第一个warp的32线程正好可以满载32个warp的结果
+  if (warp_id > 0)
+    return;
+  sum = (tid < NUM_WARPS) ? smem[tid] : CUDART_ZERO_FP16;
+  sum = warp_reduce_sum_f16_f16(sum);
+  if (tid == 0)
+    atomicAdd(y, sum);
+}
+
+template <const int NUM_THREADS = 256 / 8>
+__global__ void block_all_reduce_f16x8_pack_f16_kernel(half *x, half *y,
+                                                       int mask) {
+
+  int tid = threadIdx.x;
+  int idx = (blockDim.x * blockIdx.x + tid) * 8;
+  if (idx > mask)
+    return;
+  constexpr int NUM_WARPS = (NUM_THREADS + WARP_SIZE - 1) / WARP_SIZE;
+  __shared__ float smem[NUM_WARPS];
+  half reg_x[8];
+  LDST128BITS(reg_x) = LDST128BITS(y[idx]);
+  half sum;
+  for (int i = 0; i < 8; i++) {
+    sum = __hadd(sum, reg_x[i]);
+  }
+  sum = warp_reduce_sum_f16_f16(sum);
+  int warp_id = tid / WARP_SIZE;
+  int lane_id = tid % WARP_SIZE;
+  smem[warp_id] = sum;
+  __syncthreads();
+  if (tid > WARP_SIZE)
+    return;
+  sum = warp_id < NUM_WARPS ? smem[tid] : CUDART_ZERO_FP16;
+  sum = warp_reduce_sum_f16_f16(sum);
+  if (tid == 0)
+    atomicAdd(y, sum);
 }
