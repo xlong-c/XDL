878d9e814de340ae00674ca4d2d90fd29524b0ca
xlong <starcang@qq.com>
2026-01-28 02:27:03 +0800

    u

--- learn/cuda/kernels/softmax/softmax.cu
+++ learn/cuda/kernels/softmax/softmax.cu
@@ -1,6 +1,20 @@
+#include "cooperative_groups.h"
 #include "cuda_bf16.h"
 #include "cuda_fp16.h"
 #include "cuda_runtime.h"
-
-#define WARP_SIZE = 32
+#include <__clang_cuda_runtime_wrapper.h>
+#define WARP_SIZE 32
 #define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
+
+struct __align__(8) MD {
+  float m;
+  float d;
+};
+
+template <const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ MD warp_reduce_md_op(MD value) {
+  unsigned int mask = 0xffffffff;
+#pragma unroll
+  for (int stride = kWarpSize >> 1; stride > 1; stride >>= 1) {
+  }
+}
