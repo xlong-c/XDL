4c4b2716ab88211378744f66398b690c83a36278
xlong <starcang@qq.com>
2026-03-04 00:25:44 +0800

    feat(cuda): implement 8x8 sliced-k float4 sgemm kernel

--- learn/cuda/kernels/gemm/sgemm.cu
+++ learn/cuda/kernels/gemm/sgemm.cu
@@ -100,7 +100,8 @@ __global__ void sgemm_sliced_k_f32_kernel(float *A, float *B, float *C, int M,
   C[store_gmem_c_addr] = sum;
 }
 
-template <const int BM = 128, const int BN = 128,const int BK =8,const int TM = 8,const int TN = 8>
+template <const int BM = 128, const int BN = 128, const int BK = 8,
+          const int TM = 8, const int TN = 8>
 __global__ void sgemm_t_8x8_sliced_k_f32x4_kernel(float *A, float *B, float *C,
                                                   int M, int N, int K) {
   int tx = threadIdx.x;
@@ -108,9 +109,50 @@ __global__ void sgemm_t_8x8_sliced_k_f32x4_kernel(float *A, float *B, float *C,
   int bx = blockIdx.x;
   int by = blockIdx.y;
   int tid = blockDim.x * ty + tx;
+
   __shared__ float s_a[BM][BK];
   __shared__ float s_b[BK][BN];
-  int load_smem_a_k = tx;
-  int load_smem_b_n = ty;
+
+  int load_smem_a_m = tid / 2;
+  int load_smem_a_k = (tid % 2 == 0) ? 0 : 4;
+  int load_smem_b_k = tid / 32;
+  int load_smem_b_n = tid % 32 * 4;
+
+  int load_gmem_a_m = by * BM + load_smem_a_m;
+  int load_gmem_b_n = bx * BN + load_smem_b_n;
+  float r_c[TM][TN] = {0.0f};
+
+  for (int bk = 0; bk < (K + BK - 1) / BK; ++bk) {
+
+    int load_gmem_a_k = bk * BM + load_smem_a_k;
+    int load_gmem_a_addr = load_gmem_a_m * K + load_gmem_a_k;
+    FLOAT4(s_a[load_smem_a_m][load_smem_a_k]) = FLOAT4(A[load_gmem_a_addr]);
+    int load_gmem_b_k = bk * BK + load_smem_b_k;
+    int load_gmem_b_addr = load_gmem_b_k * N + load_gmem_b_n;
+    FLOAT4(s_b[load_smem_b_k][load_smem_b_n]) = FLOAT4(B[load_gmem_b_addr]);
+    __syncthreads();
+#pragma unroll
+    for (int k = 0; k < BK; k++) {
+#pragma unroll
+      for (int m = 0; m < TM; m++) {
+#pragma unroll
+        for (int n = 0; n < TN; n++) {
+          int comp_smem_a_m = ty * TM + m;
+          int comp_smem_b_n = tx * TN + n;
+          r_c[m][n] += s_a[comp_smem_a_m] * s_b[k][comp_smem_b_n];
+        }
+      }
+    }
+    __syncthreads();
+  }
+#pragma unroll
+  for (int m = 0; m < TM; m++) {
+    int store_gmem_c_m = by * BM + ty * TM + m;
+#pragma unroll
+    for (int n = 0; n < TN; n += 4) {
+      int store_gmem_c_n = bx * BN + tx * TN + n;
+      int storegmem_c_addr = store_gmem_c_m * N + store_gmem_c_n;
+      FLOAT4(C[storegmem_c_addr]) = FLOAT4(r_c[m][n]);
+    }
+  }
 }
- 
