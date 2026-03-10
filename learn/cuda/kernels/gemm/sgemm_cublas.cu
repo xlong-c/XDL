68f3d16c4f43da8c3b2c9fa164874088fe052bf9
xlong <starcang@qq.com>
2026-03-11 00:42:31 +0800

    feat(cuda): add cublasLt kernel fusion demo and update cublas GEMM examples

--- learn/cuda/kernels/gemm/sgemm_cublas.cu
+++ learn/cuda/kernels/gemm/sgemm_cublas.cu
@@ -16,4 +16,36 @@ void cublas_sgemm(float *A, float *B, float *C, size_t M, size_t N, size_t K) {
   cublasHandle_t handle = nullptr;
   cublasCreate(&handle);
   cublasSetMathMode(handle, CUBLAS_DEFAULT_MATH);
+
+  static float alpha = 1.0f;
+  static float beta = 0.0f;
+
+  cublasGemmEx(handle,
+               CUBLAS_OP_N, CUBLAS_OP_N,
+               M, N, K,
+               &alpha,
+               A, CUDA_R_32F, M,
+               B, CUDA_R_32F, K,
+               &beta,
+               C, CUDA_R_32F, M,
+               CUBLAS_COMPUTE_32F,
+               CUBLAS_GEMM_DEFAULT);
+}
+
+void cublas_sgemm_tf32(float *A, float *B, float *C, size_t M, size_t N, size_t K) {
+  cublasHandle_t handle = nullptr;
+  cublasCreate(&handle);
+  cublasSetMathMode(handle, CUBLAS_TF32_TENSOR_OP_MATH);
+  static float alpha = 1.0f;
+  static float beta = 0.0f;
+  cublasGemmEx(handle,
+               CUBLAS_OP_N, CUBLAS_OP_N,
+               M, N, K,
+               &alpha,
+               A, CUDA_R_32F, M,
+               B, CUDA_R_32F, K,
+               &beta,
+               C, CUDA_R_32F, M,
+               CUBLAS_COMPUTE_32F,
+               CUBLAS_GEMM_DEFAULT_TENSOR_OP);
 }
