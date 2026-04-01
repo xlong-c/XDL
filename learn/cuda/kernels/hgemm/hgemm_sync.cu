4044619fe7fa9bc8b8258fc3d423b5b35184e7ea
xlong <starcang@qq.com>
2026-04-01 18:06:17 +0800

    feat(cuda/hgemm): 更新同步内核并添加 wmma 基础内核

--- learn/cuda/kernels/hgemm/hgemm_sync.cu
+++ learn/cuda/kernels/hgemm/hgemm_sync.cu
@@ -229,12 +229,200 @@ __global__ void hgemm_8x8_slice_k32_pack_dbuf(half *A, half *B, half *C,
   int bx = blockIdx.x;
   int by = blockIdx.y;
   int tid = tx + ty * blockDim.x;
-  __shared__ half s_a[2][BK][BM];
+  __shared__ half s_a[2][BK][BM]; // 2 x 32 x 128
   __shared__ half s_b[2][BK][BN];
   half r_a_load[16];
   half r_a_buf[TM];
   half r_b_buf[TN];
   half r_c[TM][TN] = {CUDART_ZERO_FP16};
-  // 现在每个线程需要加载的数据是多少个?
-  int idx_s_am = tid / 2;
+  // 现在每个线程需要加载的数据是多少个? 128x32=4096, 4096/((128/8)*(128*8))=4096/256=16
+  int idx_s_am = tid / 2; // 32, 每次加载16个数据,一行32,需要2个线程
+  int idx_s_ak = (tid & 1) << 4;
+  int idx_s_bn = tid / 8; // 128, 128bit / 8bit = 16, 128/16=8
+  int idx_s_bk = (tid & 7) << 4;
+  int idx_g_am = idx_s_am + by * BM;
+  int idx_g_bn = idx_s_bn + bx * BN;
+  if (idx_g_am >= M || idx_g_bn >= N) {
+    return;
+  }
+  {
+    int idx_g_ak = idx_s_ak;
+    int idx_g_bk = idx_s_bk;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_ak * N + idx_g_bn;
+    LDST128BITS(r_a_load[0]) = LDST128BITS(A[addr_a + 0]);
+    LDST128BITS(r_a_load[8]) = LDST128BITS(A[addr_a + 8]);
+    uint32_t addr_b_u32 = __cvta_generic_to_shared(&s_b[0][idx_s_bk][idx_s_bn]);
+    CP_ASYNC_CA(addr_b_u32 + 0, &B[addr_b + 0], 16); // 异步加载到s_b
+    CP_ASYNC_CA(addr_b_u32 + 16, &B[addr_b + 8], 16);
+#pragma unroll
+    for (int i = 0; i < 16; i++) {
+      s_a[0][idx_s_ak + i][idx_s_am] = r_a_load[i];
+    }
+    CP_ASYNC_COMMIT_GROUP();
+  }
+  __syncthreads();
+#pragma unroll
+  for (int bk = 1; bk < K / BK; bk++) {
+    int sel_smem = (bk - 1) & 1; // 当前块的SMEM选择,错位,当前bk是1,但是是计算第0块的SMEM
+    int sel_buf = bk & 1;
+    int idx_g_ak = idx_s_ak + bk * BK;
+    int idx_g_bk = idx_s_bk + bk * BK;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_ak * N + idx_g_bn;
+    uint32_t addr_b_u32 = __cvta_generic_to_shared(&s_b[sel_buf][idx_s_bk][idx_s_bn]);
+    CP_ASYNC_CA(addr_b_u32 + 0, &B[addr_b + 0], 16);
+    CP_ASYNC_CA(addr_b_u32 + 16, &B[addr_b + 8], 16);
+    CP_ASYNC_COMMIT_GROUP();
+#pragma unroll
+    for (int tk = 0; tk < BK; tk++) {
+      LDST128BITS(r_a_buf[0]) = LDST128BITS(s_a[sel_smem][tk][ty * TM]);
+      LDST128BITS(r_b_buf[0]) = LDST128BITS(s_b[sel_smem][tk][tx * TN]);
+#pragma unroll
+      for (int tm = 0; tm < TM; tm++) {
+#pragma unroll
+        for (int tn = 0; tn < TN; tn++) {
+          r_c[tm][tn] = __hfma(r_a_buf[tm], r_b_buf[tn], r_c[tm][tn]);
+        }
+      }
+    }
+    LDST128BITS(r_a_load[0]) = LDST128BITS(A[addr_a + 0]);
+    LDST128BITS(r_a_load[8]) = LDST128BITS(A[addr_a + 8]);
+#pragma unroll
+    for (int i = 0; i < 16; i++) {
+      s_a[sel_buf][idx_s_ak + i][idx_s_am] = r_a_load[i];
+    }
+    CP_ASYNC_WAIT_GROUP(0);
+  }
+  __syncthreads();
+  {
+#pragma unroll
+    for (int tk = 0; tk < BK; tk++) {
+      LDST128BITS(r_a_buf[0]) = LDST128BITS(s_a[1][tk][ty * TM]);
+      LDST128BITS(r_b_buf[0]) = LDST128BITS(s_b[1][tk][tx * TN]);
+#pragma unroll
+      for (int tm = 0; tm < TM; tm++) {
+#pragma unroll
+        for (int tn = 0; tn < TN; tn++) {
+          r_c[tm][tn] = __hfma(r_a_buf[tm], r_b_buf[tn], r_c[tm][tn]);
+        }
+      }
+    }
+  } // 循环结束
+#pragma unroll
+  for (int tm = 0; tm < TM; tm++) {
+    int idx_g_cm = by * BM + ty * TM + tm;
+    int idx_g_cn = bx * BN + tx * TN;
+    int addr_c = idx_g_cm * N + idx_g_cn;
+    LDST128BITS(C[addr_c]) = LDST128BITS(r_c[tm][0]);
+  }
+}
+
+template <const int BM = 128, const int BN = 128, const int BK = 32,
+          const int TM = 16, const int TN = 8, const int OFFSET = 0>
+__global__ void hgemm_16x8_slice_k32_pack_dbuf_sync(half *A, half *B, half *C,
+                                                    int M, int N, int K) {
+  int tx = threadIdx.x;
+  int ty = threadIdx.y;
+  int bx = blockIdx.x;
+  int by = blockIdx.y;
+  int tid = tx + ty * blockDim.x;
+  __shared__ half s_a[2][BK][BM + OFFSET];
+  __shared__ half s_b[2][BK][BN + OFFSET];
+  half r_a_load[32]; // 线程数量128/16=8 128/8=16 8*16=128个 BK*BM=32*128=4096 4096/128=32
+  half r_a_buf[TM];
+  half r_b_buf[TN];
+  half r_c[TM][TN] = {CUDART_ZERO_FP16};
+  int idx_s_am = tid;
+  int idx_s_ak = 0;
+  int idx_s_bn = tid / 4;
+  int idx_s_bk = (tid & 7) << 5;
+  int idx_g_am = idx_s_am + by * BM;
+  int idx_g_bn = idx_s_bn + bx * BN;
+  if (idx_g_am >= M || idx_g_bn >= N) {
+    return;
+  }
+  {
+    int idx_g_ak = idx_s_ak;
+    int idx_g_bk = idx_s_bk;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_ak * N + idx_g_bn;
+    uint32_t addr_b_u32 = __cvta_generic_to_shared(&s_b[0][idx_s_bk][idx_s_bn]);
+#pragma unroll
+    for (int i = 0; i < 32; i += 8) {
+      CP_ASYNC_CA(addr_b_u32 + i, &B[addr_b + i], 16);
+    }
+    CP_ASYNC_COMMIT_GROUP();
+#pragma unroll
+    for (int i = 0; i < 32; i += 8) {
+      LDST128BITS(r_a_load[i]) = LDST128BITS(A[addr_a + i]);
+    }
+#pragma unroll
+    for (int i = 0; i < 32; i++) {
+      s_a[0][idx_s_ak + i][idx_s_am] = r_a_load[i];
+    }
+    CP_ASYNC_WAIT_GROUP(0);
+  }
+  __syncthreads();
+  for (int bk = 1; bk < K / BK; bk++) {
+    int sel_smem = (bk - 1) & 1;
+    int sel_buf = bk & 1;
+    int idx_g_ak = idx_s_ak + bk * BK;
+    int idx_g_bk = idx_s_bk + bk * BK;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_ak * N + idx_g_bn;
+    // 开始搬运b到缓存,这样在计算的时候,b可以异步的读取到缓存中
+    uint32_t addr_b_u32 = __cvta_generic_to_shared(&s_b[sel_buf][idx_s_bk][idx_s_bn]);
+#pragma unroll
+    for (int i = 0; i < 32; i += 8) {
+      CP_ASYNC_CA(addr_b_u32 + i, &B[addr_b + i], 16);
+    }
+    CP_ASYNC_COMMIT_GROUP(); // 提交
+#pragma unroll               // 开始计算
+    for (int tk = 0; tk < BK; tk++) {
+      LDST128BITS(r_a_buf[0]) = LDST128BITS(s_a[sel_smem][tk][ty * TM]);
+      LDST128BITS(r_a_buf[8]) = LDST128BITS(s_a[sel_smem][tk][ty * TM + 8]); // 这里TM变成16了,要两次加载
+      LDST128BITS(r_b_buf[0]) = LDST128BITS(s_b[sel_smem][tk][tx * TN]);
+#pragma unroll
+      for (int tm = 0; tm < TM; tm++) {
+#pragma unroll
+        for (int tn = 0; tn < TN; tn++) {
+          r_c[tm][tn] = __hfma(r_a_buf[tm], r_b_buf[tn], r_c[tm][tn]);
+        }
+      }
+    }
+    // 开始搬运a到缓存
+#pragma unroll
+    for (int i = 0; i < 32; i += 8) {
+      LDST128BITS(r_a_load[i]) = LDST128BITS(A[addr_a + i]); //因为s_a的存储不连续,这里异步搬运a不合适
+    }
+#pragma unroll
+    for (int i = 0; i < 32; i++) {
+      s_a[sel_buf][idx_s_ak + i][idx_s_am] = r_a_load[i];
+    }
+    CP_ASYNC_WAIT_GROUP(0);
+  }
+  __syncthreads();
+  { // 计算最后的块
+#pragma unroll
+    for (int tk = 0; tk < BK; tk++) {
+      LDST128BITS(r_a_buf[0]) = LDST128BITS(s_a[1][tk][ty * TM]);
+      LDST128BITS(r_a_buf[8]) = LDST128BITS(s_a[1][tk][ty * TM + 8]);
+      LDST128BITS(r_b_buf[0]) = LDST128BITS(s_b[1][tk][tx * TN]);
+#pragma unroll
+      for (int tm = 0; tm < TM; tm++) {
+#pragma unroll
+        for (int tn = 0; tn < TN; tn++) {
+          r_c[tm][tn] = __hfma(r_a_buf[tm], r_b_buf[tn], r_c[tm][tn]);
+        }
+      }
+    }
+  }
+#pragma unroll
+  for (int tm = 0; tm < TM; tm++) {
+    int idx_g_cm = by * BM + ty * TM + tm;
+    int idx_g_cn = bx * BN + tx * TN;
+    int addr_c = idx_g_cm * N + idx_g_cn;
+    LDST128BITS(C[addr_c]) = LDST128BITS(r_c[tm][0]);
+  }
 }
---
+++ learn/cuda/kernels/hgemm/hgemm_wmma.cu
@@ -0,0 +1,79 @@
+#include <__clang_cuda_runtime_wrapper.h>
+#include <algorithm>
+#include <cuda_bf16.h>
+#include <cuda_fp16.h>
+#include <cuda_fp8.h>
+#include <cuda_runtime.h>
+#include <float.h>
+#include <mma.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+using namespace nvcuda;
+
+#define WARP_SIZE 32
+#define DEVICE_INLINE __device__ inline
+#define HOST_DEVICE_INLINE __device__ __host__ inline
+#define INT4(value) (reinterpret_cast<int4 *>(&(value))[0])
+#define FLOAT4(value) (reinterpret_cast<float4 *>(&(value))[0])
+#define HALF2(value) (reinterpret_cast<half2 *>(&(value))[0])
+#define BFLOAT2(value) (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
+#define LDST32BITS(value) (reinterpret_cast<half2 *>(&(value))[0])
+#define LDST64BITS(value) (reinterpret_cast<float2 *>(&(value))[0])
+#define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
+#define CP_ASYNC_COMMIT_GROUP() asm volatile("cp.async.commit_group;\n" ::)
+#define CP_ASYNC_WAIT_ALL() asm volatile("cp.async.wait_all;\n" ::)
+#define CP_ASYNC_WAIT_GROUP(n) asm volatile("cp.async.wait_group %0;\n" ::"n"(n))
+#define CP_ASYNC_CA(dst, src, bytes) asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(dst), "l"(src), "n"(bytes))
+#define CP_ASYNC_CG(dst, src, bytes) asm volatile("cp.async.cg.shared.global.L2::128B [%0], [%1], %2;\n" ::"r"(dst), "l"(src), "n"(bytes))
+
+HOST_DEVICE_INLINE
+int div_ceil(int a, int b) {
+  return (a % b) != 0 ? ((a + b - 1) / b) : (a / b);
+}
+
+template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16>
+__global__ void hgemm_wmma_m16n16k16(const half *A, const half *B, half *C, int M, int N, int K) {
+  const int NUM_K_ITERS = div_ceil(K, WMMA_K);
+  const int idx_g_am = blockIdx.y * WMMA_M; // 当前块对应的m块索引
+  const int idx_g_an = blockIdx.x * WMMA_N;
+  if (idx_g_am >= M || idx_g_an >= N) {
+    return;
+  }
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc;
+  wmma::fill_fragment(acc, 0.0f);
+#pragma unroll
+  for (int k = 0; k < NUM_K_ITERS; ++k) {
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b;
+    wmma::load_matrix_sync(a, A + idx_g_am * K + k * WMMA_K, K);
+    wmma::load_matrix_sync(b, B + N * k * WMMA_K + idx_g_an, N);
+    wmma::mma_sync(acc, a, b, acc);
+    __syncthreads();
+  }
+  wmma::store_matrix_sync(C + idx_g_am * N + idx_g_an, acc, N, wmma::mem_row_major);
+}
+
+template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16,
+          const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2>
+__global__ void hgemm_wmma_m16n16k16_tiled_m4n2(const half *A, const half *B, half *C, int M, int N, int K) {
+  const int bx = blockIdx.x;
+  const int by = blockIdx.y;
+  const int NUM_K_ITERS = div_ceil(K, WMMA_K);
+  constexpr int BM = WMMA_M * WMMA_TILE_M; // 16 * 4 = 64
+  constexpr int BN = WMMA_N * WMMA_TILE_N; // 16 * 2 = 32
+  constexpr int BK = WMMA_K;
+  __shared__ half s_a[BM][BK];
+  __shared__ half s_b[BK][BN];
+  const int tid = threadIdx.x + threadIdx.y * blockDim.x;
+  const int warp_id = tid / WARP_SIZE;
+  const int lane_id = tid % WARP_SIZE;
+  const int warp_m = warp_id / 4;
+  const int warp_n = warp_id % 2;
+  //每个线程读取的数据, 16*16/32=4
+  const int load_smem_a_m = tid / 4; //s_a 64x16 一次4个数据,一行4个线程
+  const int load_smem_a_k = (tid % 4) * 4;
+  const int load_smem_b_k = tid / 16; // s_b 16x32 一次2个数据,一行16个线程
+  const int load_smem_b_n = (tid % 16) * 2;
+  const int load_gmem_a_m = by * BM + load_smem_a_m;
+}
