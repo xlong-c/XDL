7a11c96eb7a1aa22fac3b17a77238f30669b6103
xlong <starcang@qq.com>
2026-04-02 21:14:07 +0800

    优化hgemm内核并添加新实现

--- learn/cuda/kernels/hgemm/hgemm_wmma.cu
+++ learn/cuda/kernels/hgemm/hgemm_wmma.cu
@@ -1,13 +1,16 @@
 #include <__clang_cuda_runtime_wrapper.h>
 #include <algorithm>
+#include <cstdint>
 #include <cuda_bf16.h>
 #include <cuda_fp16.h>
 #include <cuda_fp8.h>
 #include <cuda_runtime.h>
+#include <curand_mtgp32_kernel.h>
 #include <float.h>
 #include <mma.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <vector>
 using namespace nvcuda;
 
@@ -45,7 +48,7 @@ __global__ void hgemm_wmma_m16n16k16(const half *A, const half *B, half *C, int 
 #pragma unroll
   for (int k = 0; k < NUM_K_ITERS; ++k) {
     wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b;
     wmma::load_matrix_sync(a, A + idx_g_am * K + k * WMMA_K, K);
     wmma::load_matrix_sync(b, B + N * k * WMMA_K + idx_g_an, N);
     wmma::mma_sync(acc, a, b, acc);
@@ -56,53 +59,216 @@ __global__ void hgemm_wmma_m16n16k16(const half *A, const half *B, half *C, int 
 
 template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16,
           const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2>
-__global__ void hgemm_wmma_m16n16k16_tiled_m4n2(half *A, half *B, half *C, int M, int N, int K) {
+__global__ void hgemm_wmma_m16n16k16_m4n2(half *A, half *B, half *C, int M, int N, int K) {
   const int bx = blockIdx.x;
   const int by = blockIdx.y;
-  const int NUM_K_ITERS = div_ceil(K, WMMA_K);
-  constexpr int BM = WMMA_M * WMMA_TILE_M; // 16 * 4 = 64
-  constexpr int BN = WMMA_N * WMMA_TILE_N; // 16 * 2 = 32
-  constexpr int BK = WMMA_K;
-  __shared__ half s_a[BM][BK];
-  __shared__ half s_b[BK][BN];
-  const int tid = threadIdx.x + threadIdx.y * blockDim.x;
-  const int warp_id = tid / WARP_SIZE;
-  const int lane_id = tid % WARP_SIZE;
-  const int warp_m = warp_id / 4;
+  // const int tx = threadIdx.x;
+  // const int ty = threadIdx.y;
+  const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+  const int NUM_K_ITERS = div_ceil(K, WMMA_K);  // 迭代次数
+  const int BM = WMMA_M * WMMA_TILE_M;          // 块A的行数, 16x4
+  const int BN = WMMA_N * WMMA_TILE_N;          // 块B的列数, 16x2
+  const int BK = WMMA_K;                        // 16
+  __shared__ half s_a[BM][BK], s_b[WMMA_K][BN]; //  64 x 16, 16 x 32
+  const int warp_id = tid / WARP_SIZE;          // 0~7
+  const int lane_id = tid % WARP_SIZE;          // 0~31
+  const int warp_m = warp_id / 2;
   const int warp_n = warp_id % 2;
-  //每个线程读取的数据, 16*16/32=4
-  const int load_smem_a_m = tid / 4; //s_a 64x16 一次4个数据,一行4个线程
-  const int load_smem_a_k = (tid % 4) * 4;
-  const int load_smem_b_k = tid / 16; // s_b 16x32 一次2个数据,一行16个线程
-  const int load_smem_b_n = (tid % 16) * 2;
-  const int load_gmem_a_m = by * BM + load_smem_a_m;
-  const int load_gmem_b_n = bx * BN + load_smem_b_n;
-  if (load_gmem_a_m > M || load_gmem_b_n > N)
+
+  // 加载数据
+  // 为什么每线程4数据, 数据量 64 x 16 = 1024, 1024/(8*32) = 4
+  const int idx_s_ak = (tid % 4) * 4;
+  const int idx_s_am = tid / 4; // 每行16个数据, 4数据每一个线程, 四线程每行
+  const int idx_s_bn = (tid % 16) / 2;
+  const int idx_s_bk = tid / 16; // 每个线程2个, 一行32个数据, 16线程每行
+  const int idx_g_am = by * BM + idx_s_am;
+  const int idx_g_bn = bx * BN + idx_s_bn;
+
+  if (idx_g_am >= M || idx_g_bn >= N)
     return;
 
-  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc[WMMA_TILE_M][WMMA_TILE_N];
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc;
+  wmma::fill_fragment(acc, 0.0f);
+
+#pragma unroll
+  for (int k = 0; k < NUM_K_ITERS; ++k) {
+    int idx_g_ak = idx_g_am * K + k * WMMA_K;
+    int idx_g_bk = k * WMMA_K + idx_g_bn;
+    int addr_a = idx_g_ak + idx_g_am * K;
+    int addr_b = idx_g_bk * N + idx_g_bn;
+    LDST64BITS(s_a[idx_s_am][idx_s_ak]) = LDST64BITS(A[addr_a]);
+    LDST32BITS(s_b[idx_s_bk][idx_s_bn]) = LDST32BITS(B[addr_b]);
+    __syncthreads();
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b;
+
+    wmma::load_matrix_sync(a, &s_a[warp_m * WMMA_M][0], BK);
+    wmma::load_matrix_sync(b, &s_b[0][warp_n * WMMA_N], BN);
+    wmma::mma_sync(acc, a, b, acc);
+    __syncthreads();
+  }
+  const int idx_c_m = by * BM + warp_m * WMMA_M; // 每个warp对应的m索引
+  const int idx_c_n = bx * BN + warp_n * WMMA_N;
+  wmma::store_matrix_sync(C + idx_c_m * N + idx_c_n, acc, N, wmma::mem_row_major);
+}
+
+template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16,
+          const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2,
+          const int WARP_TILE_M = 2, const int WARP_TILE_N = 4>
+__global__ void hgemm_wmma_m16n16k16_mma4x2_warp2x4_tile(half *A, half *B, half *C, int M, int N, int K) {
+  const int bx = blockIdx.x;
+  const int by = blockIdx.y;
+  const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+  const int NUM_K_ITERS = div_ceil(K, WMMA_K);
+  const int BM = WMMA_M * WMMA_TILE_M * WARP_TILE_M; // 128
+  const int BN = WMMA_N * WMMA_TILE_N * WARP_TILE_N; // 128
+  const int BK = WMMA_K;
+  __shared__ half s_a[BM][BK], s_b[BK][BN]; // 128 16, 16 128
+  const int warp_id = tid / WARP_SIZE;
+  const int lane_id = tid % WARP_SIZE;
+  const int warp_m = warp_id / WMMA_TILE_N;
+  const int warp_n = warp_id % WMMA_TILE_N;
+
+  //
+  // 数据总量 2048 线程数量256
+  int idx_s_am = tid / 2;
+  int idx_s_ak = (tid % 2) * 8;
+  int idx_s_bk = tid / 16;
+  int idx_s_bn = (tid % 16) * 8;
+
+  int idx_g_am = by * BM + idx_s_am;
+  int idx_g_bn = bx * BN + idx_s_bn;
+  if (idx_g_am >= M || idx_g_bn >= N)
+    return;
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc[WMMA_TILE_M][WARP_TILE_N];
 #pragma unroll
   for (int i = 0; i < WMMA_TILE_M; ++i) {
 #pragma unroll
-    for (int j = 0; j < WMMA_TILE_N; ++j) {
+    for (int j = 0; j < WARP_TILE_N; ++j) {
       wmma::fill_fragment(acc[i][j], 0.0f);
     }
   }
+
 #pragma unroll
   for (int k = 0; k < NUM_K_ITERS; ++k) {
-    int load_gmem_a_k = k * WMMA_K + load_smem_a_k;
-    int addr_a = load_gmem_a_m * K + load_gmem_a_k;
-    int load_gmem_b_k = k * WMMA_K + load_smem_b_k;
-    int addr_b = load_gmem_b_k * N + load_gmem_b_n;
-    LDST128BITS(s_b[load_smem_b_k][load_smem_b_n]) = LDST128BITS(B[addr_b]);
-    LDST128BITS(s_a[load_smem_a_m][load_smem_a_k]) = LDST128BITS(A[addr_a]);
+    int idx_g_ak = k * WMMA_K + idx_s_ak;
+    int idx_g_bk = k * WMMA_K + idx_s_bk;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_bk * N + idx_g_bn;
+    LDST128BITS(s_a[idx_s_am][idx_s_ak]) = LDST128BITS(A[addr_a]);
+    LDST128BITS(s_b[idx_s_bk][idx_s_bn]) = LDST128BITS(B[addr_b]);
     __syncthreads();
-    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a;
-    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::col_major> b;
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a[WARP_TILE_M];
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b[WARP_TILE_N];
 #pragma unroll
     for (int i = 0; i < WMMA_TILE_M; ++i) {
-      const int warp_smem_a_m = warp_m * WMMA_M * WMMA_TILE_M + i * WMMA_M;
-      wmma::load_matrix_sync(a, &s_a[warp_smem_a_m][0], BK);
+      int idx_a = i * WMMA_M + warp_m * (WMMA_M * WMMA_TILE_M);
+      wmma::load_matrix_sync(
+          a[i], &s_a[idx_a][0], BK);
     }
+#pragma unroll
+    for (int j = 0; j < WARP_TILE_N; ++j) {
+      int idx_b = j * WMMA_N + warp_n * (WMMA_N * WMMA_TILE_N);
+      wmma::load_matrix_sync(
+          b[j], &s_b[0][idx_b], BN);
+    }
+#pragma unroll
+    for (int i = 0; i < WMMA_TILE_M; ++i) {
+#pragma unroll
+      for (int j = 0; j < WARP_TILE_N; ++j) {
+        wmma::mma_sync(acc[i][j], a[i], b[j], acc[i][j]);
+      }
+    }
+    __syncthreads();
+  }
+#pragma unroll
+  for (int i = 0; i < WMMA_TILE_M; ++i) {
+#pragma unroll
+    for (int j = 0; j < WARP_TILE_N; ++j) {
+      int idx_c_m = by * BM + i * WMMA_M + warp_m * (WMMA_M * WMMA_TILE_M);
+      int idx_c_n = bx * BN + j * WMMA_N + warp_n * (WMMA_N * WMMA_TILE_N);
+      wmma::store_matrix_sync(C + idx_c_m * N + idx_c_n, acc[i][j], N, wmma::mem_row_major);
+    }
+  }
+}
+
+template <const int WMMA_M = 16, const int WMMA_N = 16, const int WMMA_K = 16,
+          const int WMMA_TILE_M = 4, const int WMMA_TILE_N = 2,
+          const int WARP_TILE_M = 2, const int WARP_TILE_N = 4,
+          const int OFFSET = 0>
+__global__ void hgemm_wmma_m16n16k16_mma4x2_warp2x4_dbuf_async(half *A, half *B, half *C, int M, int N, int K) {
+  const int bx = blockIdx.x;
+  const int by = blockIdx.y;
+  const int tid = threadIdx.y * blockDim.x + threadIdx.x;
+  const int NUM_K_ITERS = div_ceil(K, WMMA_K);
+  const int BM = WMMA_M * WMMA_TILE_M * WARP_TILE_M;
+  const int BN = WMMA_N * WMMA_TILE_N * WARP_TILE_N;
+  const int BK = WMMA_K;
+  const int warp_id = tid / WARP_SIZE;
+  const int lane_id = tid % WARP_SIZE;
+  const int warp_m = warp_id / WMMA_TILE_N;
+  const int warp_n = warp_id % WMMA_TILE_N;
+
+  __shared__ half s_a[2][BM][BK], s_b[2][BK][BN];
+
+  // 数据总量 2048 线程数量256, 128 * 16 = 2048
+  int idx_s_am = tid / 2;
+  int idx_s_ak = (tid % 2) * 8;
+  int idx_s_bk = tid / 16; // 128 / 8 = 16
+  int idx_s_bn = (tid % 16) * 8;
+
+  int idx_g_am = by * BM + idx_s_am;
+  int idx_g_bn = bx * BN + idx_s_bn;
+  if (idx_g_am >= M || idx_g_bn >= N)
+    return;
+
+  wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> acc[WARP_TILE_M][WARP_TILE_N];
+#pragma unroll
+  for (int i = 0; i < WARP_TILE_M; i++) {
+#pragma unroll
+    for (int j = 0; j < WARP_TILE_N; j++) {
+      wmma::fill_fragment(acc[i][j], 0.0f);
+    }
+  }
+  { // 预取
+    int idx_g_ak = idx_s_ak;
+    int idx_g_bk = idx_s_bk;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_bk * N + idx_g_bn;
+    uint32_t load_smem_a_ptr = __cvta_generic_to_shared(&s_a[0][idx_s_am][idx_s_ak]);
+    uint32_t load_smem_b_ptr = __cvta_generic_to_shared(&s_b[0][idx_s_bk][idx_s_bn]);
+    CP_ASYNC_CG(load_smem_a_ptr, &A[addr_a], 16);
+    CP_ASYNC_CG(load_smem_b_ptr, &B[addr_b], 16);
+    CP_ASYNC_COMMIT_GROUP();
+    CP_ASYNC_WAIT_GROUP(0);
+  }
+  __syncthreads();
+#pragma unroll
+  for (int k = 1; k < NUM_K_ITERS; k++) {
+    int smem_sel = (k - 1) & 1; // 当前是上一块的坐标
+    int smem_sel_next = k & 1;
+    int idx_g_ak = k * WMMA_K + idx_s_ak;
+    int idx_g_bk = k * WMMA_K + idx_s_bk;
+    int addr_a = idx_g_am * K + idx_g_ak;
+    int addr_b = idx_g_bk * N + idx_g_bn;
+
+    uint32_t load_smem_a_ptr = __cvta_generic_to_shared(&s_a[smem_sel_next][idx_s_am][idx_s_ak]);
+    uint32_t load_smem_b_ptr = __cvta_generic_to_shared(&s_b[smem_sel_next][idx_s_bk][idx_s_bn]);
+    CP_ASYNC_CG(load_smem_a_ptr, &A[addr_a], 16);
+    CP_ASYNC_CG(load_smem_b_ptr, &B[addr_b], 16);
+    CP_ASYNC_COMMIT_GROUP();
+
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a[WARP_TILE_M];
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b[WARP_TILE_N];
+#pragma unroll
+    for (int i = 0; i < WARP_TILE_M; i++) {
+      int idx_a = i * WMMA_M + warp_m * (WMMA_M * WMMA_TILE_M); // 
+      wmma::load_matrix_sync(
+          a[i], &s_a[smem_sel][idx_a][0], BK);
+    }
+
+#pragma unroll
+
+    CP_ASYNC_WAIT_GROUP(0);
   }
 }
